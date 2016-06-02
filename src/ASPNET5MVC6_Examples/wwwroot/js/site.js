29fb4809bf75352576599666007f07c5c9adf962
jpoma <jpoma@SWDVLPR16>
2016-06-01 23:12:45 -0500

    jQuery Sidebar

--- src/ASPNET5MVC6_Examples/wwwroot/js/site.js
+++ src/ASPNET5MVC6_Examples/wwwroot/js/site.js
@@ -57,4 +57,25 @@
         alert("Hello"+me.text());
     });
 
-}());
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar")
+        } else {
+            $(this).text("Hide Sidebar")
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+})();
