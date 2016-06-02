7c70352ce3a522b9c6925bc29a4cc0355bc652d3
jpoma <jpoma@SWDVLPR16>
2016-06-01 23:38:14 -0500

    Enabling MVC 6

--- src/ASPNET5MVC6_Examples/Startup.cs
+++ src/ASPNET5MVC6_Examples/Startup.cs
@@ -15,6 +15,7 @@ namespace ASPNET5MVC6_Examples
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,8 +28,18 @@ namespace ASPNET5MVC6_Examples
             //    await context.Response.WriteAsync("Hello World!");
             //});
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();            
+            //app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", action = "Index" }
+                );
+            });
+
         }
 
         // Entry point for the application.
